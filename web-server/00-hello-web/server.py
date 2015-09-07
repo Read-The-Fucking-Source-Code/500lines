039842ba4790d4d1a1977335f8234aca378c8fac
mozillazg <opensource.mozillazg@gmail.com>
2015-09-07 18:37:55 +0800

    注释

--- web-server/00-hello-web/server.py
+++ web-server/00-hello-web/server.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+"""使用内置的 BaseHTTPServer 构建一个简单的 HTTP 服务器"""
 import BaseHTTPServer
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -13,16 +18,19 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 '''
 
     # Handle a GET request.
+    # 处理一个 GET 请求
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
-        self.end_headers()
-        self.wfile.write(self.Page)
+        self.send_response(200)    # HTTP/1.1 200 OK\r\n
+                                   # Server: WSGI 1.0\r\n
+                                   # Date: Mon, 07 Sep 2015 10:18:08 GMT\r\n
+        self.send_header("Content-type", "text/html")            # Content-type: text/html\r\n
+        self.send_header("Content-Length", str(len(self.Page)))  # Content-Length: 123\r\n
+        self.end_headers()            # \r\n
+        self.wfile.write(self.Page)   # body
 
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)
     server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
-    server.serve_forever()
+    server.serve_forever()   # 启动一个死循环服务，可以通过 Ctrl + c 终止
